678b7bd3d94dbd70c2adfd2760849055a3241734
SatheeshKannanV <34863024+SatheeshKannanV@users.noreply.github.com>
2021-04-09 14:43:55 +0530

    Modify Application.java

--- wordsearch/src/main/java/com/example/wordsearch/WordsearchApplication.java
+++ wordsearch/src/main/java/com/example/wordsearch/WordsearchApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication(scanBasePackages={"com.example.wordsearch"})
+@SpringBootApplication(scanBasePackages={"com.example.wordsearch1"})
 public class WordsearchApplication {
 	
 	public static void main(String[] args) {
